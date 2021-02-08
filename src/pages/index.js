1f88625d5b661cee65ca142073abfff1b990765c
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2021-02-08 23:17:16 +0200

    seo changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const HomePage = ({ data }) => {
   return (
     <Layout
       seo={{
-        title: "Paint like a pro - Colors Frame",
+        title: "Painting tips, tricks and tools - Colors Frame",
         description: `Best tips and tricks about spray-paint, so you can transform anything in art. Get the best paint sprayers for all your paiting jobs.`,
       }}
     >
